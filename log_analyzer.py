5e5651d417b4a09aeee3b3d2c457f97b02b60f00
ibrichko <ibrichko@integralads.com>
2020-06-14 22:04:14 +0300

    14.06

--- log_analyzer.py
+++ log_analyzer.py
@@ -20,15 +20,8 @@ import string
 import shutil
 import gzip
 import statistics
+from subscription_manager.managerlib import cfg
 
-
-
-logger = logging.getLogger(__name__)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 config_path = './config.json'
 
@@ -39,6 +32,7 @@ config = {
     "LOG_DIR": "./log"
 }
 
+log_date = namedtuple('LogDate', ['date', 'path'])
 
 def process_args():
     """
@@ -57,6 +51,15 @@ def process_args():
     return parse_args
 
 
+def set_logging(cfg):
+    loging_path = cfg.get('LOGGING_FILE')
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=logging.INFO,
+        datefmt='%Y.%m.%d %H:%M:%S',
+        filename=loging_path
+    )
+
 def config_read_values(path):
     """
     Parsing config from file
@@ -71,7 +74,6 @@ def config_read_values(path):
             if path:
                 alter_config = json.loads(cfg_file.read(path))
                 read_config.update(alter_config)
-                print(read_config)
             return read_config
     except Exception as e:
         logger.info("Cannot read config file,{}".format(e))
@@ -90,25 +92,21 @@ def last_log_search(log_dir):
     Returns:
         last find logs to current date
     """
-    log_date = namedtuple('LogDate', ['date', 'path'])
     r = re.compile(r'^nginx-access-ui\.log-(?P<date>\d{8})(\.gz)?$')
     for logs in os.listdir(log_dir):
         ref = r.match(logs)
         if not ref:
             continue
-    
-        date_str = ref.groupdict()['date']
-        parsed_date = datetime.strptime(date_str, "%Y%m%d")
         try:
+            date_str = ref.groupdict()['date']
+            parsed_date = datetime.strptime(date_str, "%Y%m%d")
+        except Exception as e:
+            logging.info("Cannot initiate last log datetime {}".format(e))
             if not log_date or parsed_date > log_date.date :
                 last_log = log_date(parsed_date, os.path.join(log_dir, logs))
                 logging.info('Last log found {}'.format(log_date.path))
                 return last_log
-        except Exception as e:
-            logging.info("Log not found or {}".format(e))
-    
-    
-           
+               
 
 def process_line(line, line_number):
     """
@@ -120,7 +118,7 @@ def process_line(line, line_number):
     return {'url': url, 'request_time': request_time}
     
             
-def baselog_parser(path):
+def baselog_parser(path,refer_percent = None):
     """
     line parser to fetch urls from gzip arg
     Args:
@@ -135,8 +133,12 @@ def baselog_parser(path):
             if parsed_line:
                 handle_lines += 1
                 yield parsed_line
-        sucseccful_per = float(handle_lines) / float(total_lines) * 100
-        logging.info("sucseccful percent {}".format(sucseccful_per))
+        
+        sucseccful_per = float(handle_lines) / float(total_lines)
+        logging.info("sucseccful percent {}".format(sucseccful_per * 100))
+        if sucseccful_per < float(refer_percent):
+            raise RuntimeError('Log parser limit exceeded ')
+
                 
             
 def collect_url_data(dct):
@@ -195,14 +197,15 @@ def write_report(cfg, path, stat):
 
 
 def main(config):
-        init_report_dir(config)
+        report_dir = config.get('REPORT_DIR')
+        if not os.path.exists(report_dir):
+            os.mkdir(report_dir)
         last_log = last_log_search(config.get('LOG_DIR'))
-        print(last_log)
         report_path = os.path.join(config.get('REPORT_DIR'),
                                    'report-{0}.html'.format(datetime.strftime(last_log.date,
                                    '%Y.%m.%d'))
                                    )
-        print(report_path)
+        logging.info("report path {}".format(report_path))
         parser = baselog_parser(last_log.path, config.get('SUCSESSFUL_PERCENT')) 
         urls = collect_url_data(parser)
         stat = prepare_stat(config, urls)
@@ -212,7 +215,7 @@ def main(config):
 if __name__ == "__main__":
         alter_config = process_args()
         config = config_read_values(alter_config)
-        print(config)
+        set_logging(config)
         main(config)
 
     
